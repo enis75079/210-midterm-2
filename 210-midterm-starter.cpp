bb5fdc50f8bb4803331459021d334f1d407f2591
enis75079 <naveenislam55@gmail.com>
2024-10-17 20:18:44 -0700

    Initial commit

--- 210-midterm-starter.cpp
+++ 210-midterm-starter.cpp
@@ -210,7 +210,7 @@ int main() {
     cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
     vector<string> names;
-    ifstream file("C:/Users/Naveen Islam/projects/210-midterm-2/names.txt");
+    ifstream file("names.txt");
     string name;
     while (getline(file, name)) {
         names.push_back(name);
