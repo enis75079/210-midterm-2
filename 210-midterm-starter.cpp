1ec877a03a26bdc38a686cfd4de07854c0392f09
enis75079 <naveenislam55@gmail.com>
2024-10-17 20:58:06 -0700

    Initial commit

--- 210-midterm-starter.cpp
+++ 210-midterm-starter.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
@@ -241,26 +242,26 @@ int main() {
         cout << "Time step #" << i + 1 << endl;
 
         int prob = rand() % 100 + 1;
-        if (prob <= 40) {
+        if (rand() % 100 < 40) {
             cout << list.head->data << " is served" << endl;
             list.pop_front();
         }
 
         int prob2 = rand() % 100 + 1;
-        if (prob2 <= 60) {
+        if (rand() % 100 < 60) {
             int randName = rand() % names.size();
             cout << names[randName] << " joins the line" << endl;
             list.push_back(names[randName]);
         }
 
         int prob3 = rand() % 100 + 1;
-        if (prob3 <= 20) {
+        if (rand() % 100 < 20) {
             cout << list.tail->data << " (at the rear) left the line" << endl;
             list.pop_back();
         }
 
         int prob4 = rand() % 100 + 1;
-        if (prob4 <= 10) {
+        if (rand() % 100 < 10) {
             int randName = rand() % names.size();
             cout << names[randName] << " (VIP) joins the front of the line" << endl;
             list.push_front(names[randName]);
