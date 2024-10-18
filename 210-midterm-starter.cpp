707f216860dc2b0e6776787e4b53c1444e13b82a
enis75079 <naveenislam55@gmail.com>
2024-10-17 20:16:20 -0700

    Initial commit

--- 210-midterm-starter.cpp
+++ 210-midterm-starter.cpp
@@ -9,10 +9,10 @@ const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 class DoublyLinkedList {
 private:
     struct Node {
-        int data;
+        string data;
         Node* prev;
         Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+        Node(string val, Node* p = nullptr, Node* n = nullptr) {
             data = val; 
             prev = p;
             next = n;
@@ -25,7 +25,7 @@ private:
 public:
     DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
-    void insert_after(int value, int position) {
+    void insert_after(string value, int position) {
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
             return;
@@ -56,7 +56,7 @@ public:
         temp->next = newNode;
     }
 
-    void delete_val(int value) {
+    void delete_val(string value) {
         if (!head) return;
 
         Node* temp = head;
@@ -116,7 +116,7 @@ public:
         delete temp;
     }
 
-    void push_back(int v) {
+    void push_back(string v) {
         Node* newNode = new Node(v);
         if (!tail)
             head = tail = newNode;
@@ -127,7 +127,7 @@ public:
         }
     }
     
-    void push_front(int v) {
+    void push_front(string v) {
         Node* newNode = new Node(v);
         if (!head)
             head = tail = newNode;
@@ -210,12 +210,14 @@ int main() {
     cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
     vector<string> names;
-    ifstream file("/Users/Naveen Islam/projects/210-midterm-2/names.txt");
+    ifstream file("C:/Users/Naveen Islam/projects/210-midterm-2/names.txt");
     string name;
     while (getline(file, name)) {
         names.push_back(name);
     }
-
+    for (const auto& x : names) {
+        cout << x << endl;
+    }
     
     return 0;
 }
